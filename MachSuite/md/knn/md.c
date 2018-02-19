5055a63e5059844f642f0aa85abfb2ef32d43c39
Nils Asmussen <nils@os.inf.tu-dresden.de>
2018-02-19 11:39:37 +0100

    md-knn: generalization and added inner_loop.

--- MachSuite/md/knn/md.c
+++ MachSuite/md/knn/md.c
@@ -11,6 +11,54 @@ In Proceedings of the 3rd Workshop on General-Purpose Computation on Graphics Pr
 #include "gem5/dma_interface.h"
 #endif
 
+void inner_loop(TYPE force_x[nAtoms],
+                TYPE force_y[nAtoms],
+                TYPE force_z[nAtoms],
+                TYPE position_x[nAtoms],
+                TYPE position_y[nAtoms],
+                TYPE position_z[nAtoms],
+                int32_t NL[nAtoms*maxNeighbors],
+                int32_t i)
+{
+    TYPE delx, dely, delz, r2inv;
+    TYPE r6inv, potential, force, j_x, j_y, j_z;
+    TYPE i_x, i_y, i_z, fx, fy, fz;
+    int32_t j, jidx;
+
+     i_x = position_x[i];
+     i_y = position_y[i];
+     i_z = position_z[i];
+     fx = 0;
+     fy = 0;
+     fz = 0;
+    loop_j : for( j = 0; j < maxNeighbors; j++){
+        // Get neighbor
+        jidx = NL[i*maxNeighbors + j];
+        // Look up x,y,z positions
+        j_x = position_x[jidx];
+        j_y = position_y[jidx];
+        j_z = position_z[jidx];
+        // Calc distance
+        delx = i_x - j_x;
+        dely = i_y - j_y;
+        delz = i_z - j_z;
+        r2inv = 1.0/( delx*delx + dely*dely + delz*delz );
+        // Assume no cutoff and aways account for all nodes in area
+        r6inv = r2inv * r2inv * r2inv;
+        potential = r6inv*(lj1*r6inv - lj2);
+        // Sum changes in force
+        force = r2inv*potential;
+        fx += delx * force;
+        fy += dely * force;
+        fz += delz * force;
+    }
+    //Update forces after all neighbors accounted for.
+    force_x[i] = fx;
+    force_y[i] = fy;
+    force_z[i] = fz;
+    //printf("dF=%lf,%lf,%lf\n", fx, fy, fz);
+}
+
 void md_kernel(TYPE force_x[nAtoms],
                TYPE force_y[nAtoms],
                TYPE force_z[nAtoms],
@@ -19,10 +67,7 @@ void md_kernel(TYPE force_x[nAtoms],
                TYPE position_z[nAtoms],
                int32_t NL[nAtoms*maxNeighbors])
 {
-    TYPE delx, dely, delz, r2inv;
-    TYPE r6inv, potential, force, j_x, j_y, j_z;
-    TYPE i_x, i_y, i_z, fx, fy, fz;
-    int32_t i, j, jidx;
+    int32_t i;
 
 #ifdef DMA_MODE
     dmaLoad(&position_x[0], 0, nAtoms * sizeof(TYPE));
@@ -39,42 +84,14 @@ void md_kernel(TYPE force_x[nAtoms],
 #endif
 
 loop_i : for (i = 0; i < nAtoms; i++){
-             i_x = position_x[i];
-             i_y = position_y[i];
-             i_z = position_z[i];
-             fx = 0;
-             fy = 0;
-             fz = 0;
-loop_j : for( j = 0; j < maxNeighbors; j++){
-             // Get neighbor
-             jidx = NL[i*maxNeighbors + j];
-             // Look up x,y,z positions
-             j_x = position_x[jidx];
-             j_y = position_y[jidx];
-             j_z = position_z[jidx];
-             // Calc distance
-             delx = i_x - j_x;
-             dely = i_y - j_y;
-             delz = i_z - j_z;
-             r2inv = 1.0/( delx*delx + dely*dely + delz*delz );
-             // Assume no cutoff and aways account for all nodes in area
-             r6inv = r2inv * r2inv * r2inv;
-             potential = r6inv*(lj1*r6inv - lj2);
-             // Sum changes in force
-             force = r2inv*potential;
-             fx += delx * force;
-             fy += dely * force;
-             fz += delz * force;
-         }
-         //Update forces after all neighbors accounted for.
-         force_x[i] = fx;
-         force_y[i] = fy;
-         force_z[i] = fz;
-         //printf("dF=%lf,%lf,%lf\n", fx, fy, fz);
-         }
+        inner_loop(force_x, force_y, force_z,
+                   position_x, position_y, position_z,
+                   NL, i);
+    }
+
 #ifdef DMA_MODE
-    dmaStore(&force_x[0], 0, 256 * sizeof(TYPE));
-    dmaStore(&force_y[0], 0, 256 * sizeof(TYPE));
-    dmaStore(&force_z[0], 0, 256 * sizeof(TYPE));
+    dmaStore(&force_x[0], 0, nAtoms * sizeof(TYPE));
+    dmaStore(&force_y[0], 0, nAtoms * sizeof(TYPE));
+    dmaStore(&force_z[0], 0, nAtoms * sizeof(TYPE));
 #endif
 }
